b18403efa0674a2ea46dff826c53118ff596bda2
Deivid <deivid.cardos@gmail.com>
2023-05-11 10:38:59 -0300

    remove: unnecessary classes

--- Inventory.Domain/Interfaces/Services/IAuthService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Inventory.Domain.Entities;
-
-namespace Inventory.Domain.Interfaces.Services;
-
-public interface IAuthService
-{
-  public Task<(bool IsLogged, dynamic Data)> Login(User user);
-}
